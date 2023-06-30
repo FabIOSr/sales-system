1ba1dcedefb5a424c7abe49884b26f9d52cf8955
Fabio <fabio-s-ramos@hotmail.com>
2023-06-30 18:28:53 -0300

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header bg-white">{{ __('Dashboard') }}</div>
+
+                <div class="card-body bg-white">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
